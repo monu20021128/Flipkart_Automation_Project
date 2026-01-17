ce19c08c72ac35ea547bc64f4fb81d0eaab3072a
Monu Sahu <monusahu2811@gmail.com>
2026-01-17 08:15:45 +0530

    Test Class updated

--- src/test/java/Test/TestFlipkart.java
+++ src/test/java/Test/TestFlipkart.java
@@ -24,7 +24,7 @@ import PageClasses.SignUpPage;
 
 public class TestFlipkart extends BaseClass {
 
-	@Test
+//	@Test
 	public void Test1() throws InterruptedException, IOException {
 		
 		logger=report.createTest("Test One");
@@ -140,6 +140,55 @@ public class TestFlipkart extends BaseClass {
 		product2.product2();
 	}
 	
+	@Test
+	public void TEST() throws InterruptedException, IOException {
+		logger=report.createTest("Test One");
+		BaseClass base = new BaseClass();
+		
+		logger.log(Status.INFO, "Initializing the Browser");
+		base.openBrowser("chrome");
+		logger.log(Status.INFO, "Opening Flipkart");
+		HomePage home = base.openFlipkart();
+		logger.log(Status.INFO, "Searching Product1");
+		ProductsPage product1 = home.searchProduct1();
+		logger.log(Status.INFO, "Applying Filters and Selecting Product1");
+		product1.applyFiltersForProduct1("boAt");
+		logger.log(Status.INFO, "Navigated to Product1 Page and added product to Cart");
+		HomePage home2 = product1.product1();
+		home2.closeLoginWindow();
+		logger.log(Status.INFO, "Searching Product2");
+		ProductsPage product2 = home2.searchProduct2();
+		logger.log(Status.INFO, "Applying Filters and Selecting Product2");
+		product2.applyFiltersForProduct2();
+		logger.log(Status.INFO, "Navigated to Product2 Page and added product to Cart");
+		HomePage home4 = product2.product2();
+		logger.log(Status.INFO, "Navigating to SignUp Page");
+		SignUpPage signUp = home4.GoToSignUp();
+		logger.log(Status.INFO, "Verifying SignUp Page");
+		signUp.verifySignUpPage();
+		logger.log(Status.INFO, "Click login link from SignUp Page");
+		LoginPage login = signUp.clickLoginLink();
+		logger.log(Status.INFO, "Verifying Login Page");
+		login.verifyLoginPage();
+		logger.log(Status.INFO, "Logging in to the application");
+		HomePage home5 = login.Login();
+		logger.log(Status.INFO, "Navigating to Cart");
+		MyShoppingCart cart = home4.MyCart();
+		logger.log(Status.INFO, "Verifying Cart Page");
+		try {
+			cart.verifyCartPage();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		logger.log(Status.INFO, "Removing a product from Cart");
+		HomePage home6= cart.removeProductsFromCart();
+		logger.log(Status.INFO, "Navigating to Flights Booking Page");
+		FlightsBookings flight=home6.goToFlights();
+		logger.log(Status.INFO, "Verifying Flights Booking Page and Booking a Ticket");
+		flight.verifyFlightsPageAndBookTicket();	
+		logger.log(Status.FAIL, "Test Case Failed");
+	}
 	
 	@AfterMethod
     public void flush() {
